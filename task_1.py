2541973ff9ebc3b2da99c9a9f9fe723089d59726
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-10-26 14:56:18 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,9 @@
 def cross(n):
-    pass
+    for i in range(n):
+        print(n * " ", n * "*", n * " ", sep = "")
+    for i in range(n):
+        print(n * "*", n * "*", n * "*", sep = "")
+    for i in range(n):
+        print(n * " ", n * "*", n * " ", sep = "")
+
+cross(4)
