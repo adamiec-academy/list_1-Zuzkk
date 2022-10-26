8d99153d60203c49bc2f9317476b4fe4b3a44926
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-10-26 19:30:27 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,13 @@
 def snowball(n, k):
-    pass
-
+    for i in range(n // 2):
+        print(k * " " + (n // 2 - 1 - i) * " " + (n - 2 * (n // 2 - 1 - i)) * "#" + (n // 2 - 1 - i) * " ")
+    print(k * " " + n * "#")
+    for i in range(n // 2):
+        print(k * " " + i * " " + (n - 2 * i) * "#" + i * " ")
 
 def snowman(n):
-    pass
+    snowball(n, 2)
+    snowball(n + 2, 1)
+    snowball(n + 4, 0)
+    
+snowman(7)
