1eeb70b2a331453a563c384f4d267095dd1f060d
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-10-26 14:56:49 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,18 @@
-def chess_board(n, k):
-    pass
+def chess_board(n,k):
+    for h in range (n):
+        for i in range(k):
+            for j in range (2 * n):
+                if j % 2 == 0:
+                    print(k*" ", end="")
+                else:
+                    print(k*"#", end="")
+            print()
+        for i in range(k):
+            for j in range (2 * n):
+                if j % 2 != 0:
+                    print(k*" ", end="")
+                else:
+                    print(k*"#", end="")
+            print()
+
+chess_board(4,2)
